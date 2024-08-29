1ddfe641f77c1450e94bbcc3493f3bf36cb82204
radhika2658 <114528535+radhika2658@users.noreply.github.com>
2024-08-28 23:47:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    alert('Thank you for your message, ' + document.getElementById('name').value + '! We will get back to you soon.');
+    alert('Thank you for your message, ' + document.getElementById('name').value + '! I will get back to you soon.');
 });
